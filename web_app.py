cbced50da3baeb6fbc68d0377077364c3e4a5d61
Fractal-analysis-of-surface <63148857+Fractal-analysis-of-surface@users.noreply.github.com>
2022-02-28 18:15:51 +0100

    1_

---
+++ web_app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 28 17:39:42 2022
+
+@author: tomasic
+"""
+
+# A simple script to calculate BMI
+from pywebio.input import input, FLOAT
+from pywebio.output import put_text
+
+def bmi():
+    height = input("Input your height(cm)：", type=FLOAT)
+    weight = input("Input your weight(kg)：", type=FLOAT)
+
+    BMI = weight / (height / 100) ** 2
+
+    top_status = [(16, 'Severely underweight'), (18.5, 'Underweight'),
+                  (25, 'Normal'), (30, 'Overweight'),
+                  (35, 'Moderately obese'), (float('inf'), 'Severely obese')]
+
+    for top, status in top_status:
+        if BMI <= top:
+            put_text('Your BMI: %.1f. Category: %s' % (BMI, status))
+            break
+
+if __name__ == '__main__':
+    bmi()
